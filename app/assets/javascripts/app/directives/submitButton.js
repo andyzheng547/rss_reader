12149f695cbed321e01402c2a28a75f9c0a54dc7
azheng249 <andy.zheng249@gmail.com>
2016-06-27 19:02:39 -0400

    delete directive for submit button. each user form is different and the values should differ

--- app/assets/javascripts/app/directives/submitButton.js
+++
@@ -1,9 +0,0 @@
-function submitButton(){
-  return {
-    require: '^form',
-    restrict: 'E',
-    templateUrl: 'partials/submitButton.html'
-  }
-}
-
-angular.module('RssReaderApp').directive('submitButton', submitButton);
