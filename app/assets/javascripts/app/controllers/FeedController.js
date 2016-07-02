3db23f4d25d9b5007eeed9d96437f005fee2cca2
azheng249 <andy.zheng249@gmail.com>
2016-07-02 00:13:18 -0400

    added iframe onload listener to loadCurrentFeed function. attaches a listener that makes basic checks to see if iframe has content. if no content the iframe's source probably denied access in its X-Frame-Options

--- app/assets/javascripts/app/controllers/FeedController.js
+++ app/assets/javascripts/app/controllers/FeedController.js
@@ -1,32 +1,61 @@
 function FeedController($http, $state, $sce, SharedDataService, GoogleFeedsService) {
   var self = this;
 
-  self.currentRssUrl = SharedDataService.getCurrentFeedUrl();
   self.userFeeds = SharedDataService.getCurrentUserFeeds();
 
-  self.setCurrentLink = function(link){
-    debugger;
-    self.currentLink = link;
+  // Add onload listener to iframe to check for same origin error
+  self.attachIframeListener = function(){
+    var iframe = document.getElementById('iframe');
+    iframe.onload = function(){
+      // Try checking iframe contentWindow
+      // If length is greater than 0, then it is loaded,
+      // otherwise it is probably the source's X-Frame-Options denying access
+      try {
+        var iframeWindow = iframe.contentWindow;
+        if (iframeWindow.length > 0) {
+          console.log('iframe loaded: ' + self.currentLink);
+        } else {
+          console.log('iframe could not load: ' + self.currentLink);
+          alert('iframe could not be loaded. source refused to display.')
+        }
+      }
+
+      // Just in case of error
+      catch(e) {
+        alert('error loading iframe: ' + e);
+      }
+    };
   };
 
+  // Gets when current rss feed when state is feed.read
   self.loadCurrentFeed = function(){
-    debugger;
-    if ($state.current.name === "feed.read"){
-      GoogleFeedsService.loadRssFeed(self.currentRssUrl).then(function(resp){
-        self.currentFeed = resp.data.responseData.feed.entries;
-        self.currentLink = $sce.trustAsResourceUrl(self.currentFeed[0].link);
-      });
-    }
+    // User Google Feed API to find current posts from that rss feed
+    GoogleFeedsService.loadRssFeed(self.currentRssUrl).then(function(resp){
+      self.attachIframeListener();
+      self.currentFeed = resp.data.responseData.feed.entries;
+      self.currentLink = $sce.trustAsResourceUrl(self.currentFeed[0].link);
+    });
   };
 
+  // Called from feed.read. Will reset currentLink, which is the iframes src
   self.setCurrentLink = function(link){
     self.currentLink = $sce.trustAsResourceUrl(link);
   };
 
+  // Called when going to feed.read
   self.goToFeed = function(feed){
     SharedDataService.setCurrentFeedUrl(feed.rss_link);
 
     $state.go('feed.read');
+  };
+
+  // Takes query from feed.create and finds rss feeds using Google Feed API
+  self.discoverFeeds = function(query){
+    GoogleFeedsService.findRssFeed(query).then(function(resp){
+      self.googleRssResults = resp.data.responseData.entries;
+      console.log('You got this back from Google: \n\t' + JSON.stringify(resp));
+      alert('You got this back from Google: \n\t' + JSON.stringify(resp))
+    });
   };
 
   // this.createFeed = function(feedParams){
@@ -54,17 +83,10 @@ function FeedController($http, $state, $sce, SharedDataService, GoogleFeedsServi
   // };
   //
 
-
-  self.discoverFeeds = function(query){
-    GoogleFeedsService.findRssFeed(query).then(function(resp){
-      // debugger;
-      self.googleRssResults = resp.data.responseData.entries;
-      console.log('You got this back from Google: \n\t' + JSON.stringify(resp));
-      alert('You got this back from Google: \n\t' + JSON.stringify(resp))
-    });
-  };
-
-  self.loadCurrentFeed();
+  if ($state.current.name === "feed.read"){
+    self.currentRssUrl = SharedDataService.getCurrentFeedUrl();
+    self.loadCurrentFeed();
+  }
 
 }
 
