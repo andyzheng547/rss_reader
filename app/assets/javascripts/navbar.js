10c8f5a7859a4c742cec55f51a98e83dc53f587f
azheng249 <andy.zheng249@gmail.com>
2016-07-06 14:33:01 -0400

    styled app with colors

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,6 @@
+// Collapse navbar when link is clicked
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
